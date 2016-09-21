db4bd274ab51717ea8049335de3f4a109e2750e7
Ryan Running <ryanrunning@securitymetrics.com>
2016-09-20 20:54:52 -0600

    Adding arrays with boy and girl names and starting function.

--- baby-name.js
+++ baby-name.js
@@ -2,7 +2,11 @@
 var firstName = "First";
 var middleName = "Middle";
 var lastNameInput = document.getElementById('last-name');
+var boyNames = ['Samuel', 'Robert', 'Mark', 'Ryan', 'Paul', 'Joseph', 'David', 'Jacob', 'Benjamin', 'Jonathan', 'Matthew', 'Alexander', 'Cesar', 'John', 'Patrick', 'Christopher', 'Richard', 'Stanley', 'James', 'Kyle'];
+var girlNames = ['Sarah', 'Kimberly', 'Amanda', 'Emily', 'Megan', 'Hope', 'Mia', 'Caitlyn', 'Alexandra', 'Emma', 'Hayley', 'Christina', 'Nikki', 'Natalie', 'Sandy', 'Heather', 'Mallory', 'Lauren', 'Elizabeth', 'Kylie'];
 
+getElementByName('gender')
+if (getElementById('boy'))
 
 function addNames(){
 	var lastName = lastNameInput.value;
