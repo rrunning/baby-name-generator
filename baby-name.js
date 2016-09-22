66233d27f4419c43f9e01ef417727cec2fb3787f
Ryan Running <ryanrunning@securitymetrics.com>
2016-09-21 22:02:42 -0600

    adding function randomName to generate name.

--- baby-name.js
+++ baby-name.js
@@ -1,16 +1,23 @@
 
-var firstName = "First";
-var middleName = "Middle";
+var firstName = randomName();
+var middleName = "middle";
 var lastNameInput = document.getElementById('last-name');
 var boyNames = ['Samuel', 'Robert', 'Mark', 'Ryan', 'Paul', 'Joseph', 'David', 'Jacob', 'Benjamin', 'Jonathan', 'Matthew', 'Alexander', 'Cesar', 'John', 'Patrick', 'Christopher', 'Richard', 'Stanley', 'James', 'Kyle'];
 var girlNames = ['Sarah', 'Kimberly', 'Amanda', 'Emily', 'Megan', 'Hope', 'Mia', 'Caitlyn', 'Alexandra', 'Emma', 'Hayley', 'Christina', 'Nikki', 'Natalie', 'Sandy', 'Heather', 'Mallory', 'Lauren', 'Elizabeth', 'Kylie'];
 
-getElementByName('gender')
-if (getElementById('boy'))
+
 
 function addNames(){
-	var lastName = lastNameInput.value;
+	var lastName = "Running";
 	document.getElementById("baby-name").innerHTML = firstName + ' ' + middleName + ' ' + lastName;
+};
+
+function randomName(){
+	if (document.getElementById('boy').checked){
+		return Math.floor(Math.random() * boyNames.length);
+	} else {
+		return Math.floor(Math.random() * girlNames.length);
+	};
 };
 
 
