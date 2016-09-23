7b6195c50f5db8f92e3a9f9e6b42904e940cd6e4
Ryan Running <ryanrunning@securitymetrics.com>
2016-09-22 20:34:52 -0600

    adding css. adding functionality for random name generation.

--- baby-name.js
+++ baby-name.js
@@ -1,23 +1,51 @@
-
-var firstName = randomName();
-var middleName = "middle";
-var lastNameInput = document.getElementById('last-name');
 var boyNames = ['Samuel', 'Robert', 'Mark', 'Ryan', 'Paul', 'Joseph', 'David', 'Jacob', 'Benjamin', 'Jonathan', 'Matthew', 'Alexander', 'Cesar', 'John', 'Patrick', 'Christopher', 'Richard', 'Stanley', 'James', 'Kyle'];
 var girlNames = ['Sarah', 'Kimberly', 'Amanda', 'Emily', 'Megan', 'Hope', 'Mia', 'Caitlyn', 'Alexandra', 'Emma', 'Hayley', 'Christina', 'Nikki', 'Natalie', 'Sandy', 'Heather', 'Mallory', 'Lauren', 'Elizabeth', 'Kylie'];
 
-
-
 function addNames(){
+	var arrayGoodness = getNameArray();
+	if (!arrayGoodness){
+		alert('Please select whether you want to see boy names or girl names.')
+		return;
+	}
+	if(document.getElementById('last-name').value == ""){
+		alert('Please enter a last name.');
+		return;
+	}
+	var lastNameInput = document.getElementById('last-name').value;
 	var lastName = lastNameInput;
+	var firstName = random(arrayGoodness);
+	var middleName = random(arrayGoodness);
 	document.getElementById("baby-name").innerHTML = firstName + ' ' + middleName + ' ' + lastName;
-};
+	
+	
+}
 
-function randomName(){
+function random(ray){
+	return ray[Math.floor(Math.random() * ray.length)]
+}
+
+function getNameArray(){
 	if (document.getElementById('boy').checked){
-		return Math.floor(Math.random() * boyNames.length);
+		return boyNames;
+	} else if (document.getElementById('girl').checked){
+		return girlNames;
 	} else {
-		return Math.floor(Math.random() * girlNames.length);
-	};
-};
+		return;
+	}
+
+}
+
+function changeColor(){
+	if(document.getElementById('boy').checked){
+		document.body.style.backgroundColor = "#80bfff";
+	} else if(document.getElementById('girl').checked){
+		document.body.style.backgroundColor = "#ffb3d9"; 
+	} else {
+		return;
+	}
+}
+
+
+
 
 
